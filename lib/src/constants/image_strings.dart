540781d1e8ddeeff3a3c929c71343698ae405aa3
lujainbakh <lujainbakhurji@gmail.com>
2025-01-29 22:37:55 +0300

    started working on the boarding pages

---
+++ lib/src/constants/image_strings.dart
@@ -0,0 +1,4 @@
+// Onboarding Images
+const String onboardingimage1 = "assets/images/onboarding/onboarding1.png";
+const String onboardingimage2 = "assets/images/onboarding/onboarding2.png";
+const String onboardingimage3 = "assets/images/onboarding/onboarding3.png";
---
+++ lib/src/constants/text_strings.dart
@@ -0,0 +1,21 @@
+// Onboarding Texts
+
+// Title
+const String onboardingtitle1 = "Discover Knowledge with Ease";
+const String onboardingtitle2 = "Stay Connected Anywhere";
+const String onboardingtitle3 = "Empower Your Learning Journey";
+
+// Subtitle
+const String onboardingsubtitle1 =
+    "Find your questions and find the resources you need instantly.";
+const String onboardingsubtitle2 =
+    "Get support for your studies no matter where you are, even on the go.";
+const String onboardingsubtitle3 =
+    "Explore tools and insights tailored to your academic and personal growth.";
+
+// Boarding counter
+const String boardingcounter1 = "1/3";
+const String boardingcounter2 = "2/3";
+const String boardingcounter3 = "3/3";
+
+const double value1 = 30.0;
